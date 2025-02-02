7d422e942cf8b42ca04a52f21a3516628eda6195
SeungMin Son <sonsmmm@naver.com>
2025-02-02 17:23:20 +0900

    백준 1213번 문제

--- abc.js
+++ abc.js
@@ -1,35 +1,39 @@
 const fs=require('fs');
-const input=fs.readFileSync('input.txt').toString().trim().split('\n');
+const input=fs.readFileSync('input.txt').toString().trim().split('');
 
-const num=Number(input[0]);
-const arr=[];
-
-for(let i=1; i<=num; i++){
-    const time = input[i];
-    const[start, end]=time.split(' ').map(Number);
-    arr.push({time:start, isStart:1});
-    arr.push({time:end, isStart: -1});
-}
-
-const sortedArr = arr.sort((a,b) => {
-    if(a.time===b.time){
-        //시간이 같을 경우
-        return a.isStart - b.isStart;
-    }
-    return a.time - b.time;
-})
-
-let maxcnt=0;
-let curcnt=0;
-
-for(const arr of sortedArr){
-    if(arr.isStart===1){
-        curcnt++;
+//문자열 개수 체크
+const strCnt={};
+//펠린드롬이 될 수 있는지 확인
+for(char of input){
+    if(strCnt[char]===undefined){
+        strCnt[char]=1;
     }
     else{
-        curcnt--;
+        strCnt[char]++;
     }
-    maxcnt=Math.max(maxcnt, curcnt);
 }
 
-console.log(maxcnt);
+const sortedKeys= Object.keys(strCnt).sort();
+
+let oddcnt=0;
+let center='';
+let leftstr='';
+
+for (let char of sortedKeys){
+    const cnt = strCnt[char];
+    //홀수
+    if(cnt %2===1){
+        oddcnt++;
+        center=char;
+    }
+    leftstr += char.repeat(Math.floor(cnt/2));
+}
+if(oddcnt>1){
+    console.log("I'm Sorry Hansoo");
+}
+else{
+    let ans = leftstr + center + leftstr.split('').reverse().join('');
+
+    console.log(ans);
+}
+
