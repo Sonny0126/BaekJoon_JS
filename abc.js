001bbec119a5654631ff9b8b87f61749dd6e8994
SeungMin Son <sonsmmm@naver.com>
2025-02-03 19:56:00 +0900

    백준 1449 문제풀이

--- abc.js
+++ abc.js
@@ -1,39 +1,15 @@
 const fs=require('fs');
-const input=fs.readFileSync('input.txt').toString().trim().split('');
+const input=fs.readFileSync('input.txt').toString().trim().split('\n');
+const [N, L]=input[0].split(' ').map(Number);
+const arr=input[1].split(' ').map(Number).sort((a,b)=> a-b);
 
-//문자열 개수 체크
-const strCnt={};
-//펠린드롬이 될 수 있는지 확인
-for(char of input){
-    if(strCnt[char]===undefined){
-        strCnt[char]=1;
+let start=arr[0];
+let cnt=1; //최소한 1개의 테이프 필요
+for(let i=1; i<N; i++){
+    if(1 + arr[i] - start > L){
+        cnt++;
+        start=arr[i];``
     }
-    else{
-        strCnt[char]++;
-    }
 }
 
-const sortedKeys= Object.keys(strCnt).sort();
-
-let oddcnt=0;
-let center='';
-let leftstr='';
-
-for (let char of sortedKeys){
-    const cnt = strCnt[char];
-    //홀수
-    if(cnt %2===1){
-        oddcnt++;
-        center=char;
-    }
-    leftstr += char.repeat(Math.floor(cnt/2));
-}
-if(oddcnt>1){
-    console.log("I'm Sorry Hansoo");
-}
-else{
-    let ans = leftstr + center + leftstr.split('').reverse().join('');
-
-    console.log(ans);
-}
-
+console.log(cnt);
