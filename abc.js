85fa4a07fdff5736bfaf96a0b8929f4b9b709ee3
SeungMin Son <sonsmmm@naver.com>
2025-02-12 16:52:47 +0900

    백준 2842 풀이

--- abc.js
+++ abc.js
@@ -1,22 +1,8 @@
-const fs=require('fs');
-const input=fs.readFileSync('input.txt').toString().trim().split('\n');
-const n = Number(input[0]);
-const arr=input[1].split(' ').map(Number);
-
-const ans = new Array(n).fill(null);
-
-for(let i=0; i<n; i++){
-    let position = arr[i];
-    let cnt=0;
-    for(let j=0; j<n; j++){
-        if(ans[j]===null){
-            if(cnt===position){
-                ans[j]=i+1;
-                break;
-            }
-            cnt++;
-        }
-    }
+let count = 5;
+let sum=1;
+while(count>0){
+    sum *= count;
+    count--;
 }
 
-console.log(ans.join(' '));
+console.log(sum);
