a22b074b915f4cf59ebab5bf6d5f7cf681a04379
Son Seung Min <sonsmmm@naver.com>
2025-02-24 18:31:57 +0900

    백준 18310풀이

--- abc.js
+++ abc.js
@@ -1,31 +1,8 @@
-const fs = require('fs');
-const input = fs.readFileSync('input.txt').toString().trim().split('\n');
-const [R, C] = input[0].split(' ').map(Number);
+const x = [12, 37, 5, 42, 19];
+const y = [28, 4, 33, 21, 50];
+const z = [47, 13, 9, 36, 22];
 
-//map을 만들기
-const map=input.slice(1, R+1).map(line => line.split(''));
-const move=[[-1,1],[0,1],[1,1]];
-let cnt=0;
+//  위의 배열들 중 가장 큰 수를 출력하는 가장 간결한 코드를 작성해보세요.
 
-// 깊이 우선 탐색(DFS) 함수
-function dfs(r, c) {
-    if (c === C - 1) return true; // 마지막 열 도착 시 성공
-
-    for (let [x, y] of move) {
-        let nr = r + x, nc = c + y;
-        if (nr >= 0 && nr < R && map[nr][nc] === '.') {
-            map[nr][nc] = 'x'; // 방문 처리
-            if (dfs(nr, nc)) return true; // 성공 시 종료
-        }
-    }
-
-    return false; // 경로가 없으면 실패
-}
-
-for(let i=0; i<R; i++){
-    if(dfs(i,0)){
-        cnt++;
-    }
-}
-
-console.log(cnt);
+const max=Math.max(...x,...y,...z);
+console.log(max);
