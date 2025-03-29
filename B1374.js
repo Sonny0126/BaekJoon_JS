d46327bbcd2ce8e46a4c7249c88ad68bede65624
Son Seung Min <sonsmmm@naver.com>
2025-03-29 18:51:18 +0900

    백준 1374 풀이

--- B1374.js
+++ B1374.js
@@ -1,77 +1,79 @@
-//N개 강의실
-// 강의 번호, 강의 시작시간, 강의 종료 시간
-const fs=require('fs');
-const input=fs.readFileSync('input.txt').toString().trim().split('\n');
+const fs = require('fs');
+const input = fs.readFileSync('input.txt').toString().trim().split('\n');
 
-const N=Number(input[0]);
-let arr=[];
+const N = Number(input[0]);
+let arr = [];
 
-for(let i=1; i<=N; i++){
-    let[Gnum, Gstart, Gend]=input[i].split(' ').map(Number);
-    arr.push({num:Gnum, start:Gstart, end:Gend});
+for (let i = 1; i <= N; i++) {
+    let [Gnum, Gstart, Gend] = input[i].split(' ').map(Number);
+    arr.push({ num: Gnum, start: Gstart, end: Gend });
 }
 
-arr.sort((a,b)=> a.start-b.start);
-for(let i=0; i<N; i++)
-    console.log(arr[i]);
+// 시작 시간 기준으로 정렬
+arr.sort((a, b) => a.start - b.start);
 
-let cnt=0;
-let starttime=0;
-let classCnt=0;
-
-class MinHeap{
-    construcor(){
-        this.heap=[];
+class MinHeap {
+    constructor() {
+        this.heap = [null]; // 1-based index 사용
     }
-    swap(a,b){
-        [this.heap[a], this.heap[b]]=[this.heap[b], this.heap[a]];
+
+    insert(item) {
+        this.heap.push(item);
+        let current = this.heap.length - 1;
+        while (current > 1) {
+            let parent = Math.floor(current / 2);
+            if (this.heap[parent] > this.heap[current]) {
+                [this.heap[parent], this.heap[current]] = [this.heap[current], this.heap[parent]];
+                current = parent;
+            } else break;
+        }
     }
-    size() {
-		return this.heap.length;
-	}
 
-	push(value) {
-		this.heap.push(value);
-		let current = this.heap.length - 1;
-		let parent = Math.floor((current - 1) / 2);
+    remove() {
+        if (this.heap.length === 1) return null; // 빈 힙이면 종료
+        if (this.heap.length === 2) return this.heap.pop(); // 원소 하나면 바로 pop
 
-		while (this.heap[parent] > value) {
-			this.swap(parent, current);
-			current = parent;
-			parent = Math.floor((current - 1) / 2);
-		}
-	}
+        const min = this.heap[1];
+        this.heap[1] = this.heap.pop(); // 마지막 원소를 루트로 올림
+        let current = 1;
 
-	pop() {
-		const last = this.heap.length - 1;
-		let current = 0;
-		this.swap(current, last); // 0번이 루트노드
-		const value = this.heap.pop();
+        while (true) {
+            let left = current * 2;
+            let right = current * 2 + 1;
+            let smallest = current;
 
-		while (current < last) {
-			let left = current * 2 + 1;
-			let right = current * 2 + 2;
+            if (left < this.heap.length && this.heap[left] < this.heap[smallest]) {
+                smallest = left;
+            }
+            if (right < this.heap.length && this.heap[right] < this.heap[smallest]) {
+                smallest = right;
+            }
+            if (smallest === current) break;
 
-			if (left >= last) {
-				break;
-			} else if (right >= last) {
-				if (this.heap[current] > this.heap[left]) {
-					this.swap(current, left);
-					current = left;
-				} else {
-					break;
-				}
-			} else {
-				if (this.heap[left] < this.heap[current] || this.heap[right] < this.heap[current]) {
-					let next = this.heap[left] < this.heap[right] ? left : right;
-					this.swap(current, next);
-					current = next;
-				} else {
-					break;
-				}
-			}
-		}
-		return value;
-	}
+            [this.heap[current], this.heap[smallest]] = [this.heap[smallest], this.heap[current]];
+            current = smallest;
+        }
+        return min;
+    }
+
+    getMin() {
+        return this.heap.length > 1 ? this.heap[1] : null;
+    }
+
+    getSize() {
+        return this.heap.length - 1;
+    }
 }
 
+// 강의실 배정 알고리즘
+let PQ = new MinHeap();
+PQ.insert(arr[0].end); // 첫 번째 강의의 종료 시간을 넣음
+
+for (let i = 1; i < arr.length; i++) {
+    if (arr[i].start >= PQ.getMin()) {
+        PQ.remove();
+    }
+    PQ.insert(arr[i].end);
+}
+
+console.log(PQ.getSize());
